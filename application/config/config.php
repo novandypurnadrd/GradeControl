dbb4ba2b77f46091917e06fb056cee80b2a9f04f
novandypurnadrd <novandypurnadrd@gmail.com>
2018-06-23 08:11:34 +0700

    Config Tes

--- application/config/config.php
+++ application/config/config.php
@@ -25,6 +25,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/GradeControl/';
 
+// tes
+
 /*
 |--------------------------------------------------------------------------
 | Index File
